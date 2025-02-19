014c1318d5aac8e4a250163ef78f304e6739e57a
Manuel Garcia <gilbert.mg22@gmail.com>
2025-02-19 16:42:08 +0100

    load base_plugins as part in plugnins package

---
+++ src/sedtrails/converters/plugins/__init__.py
@@ -0,0 +1,7 @@
+from .physics.plugin import BasePhysicsPlugin
+from .format.plugin import BaseFormatPlugin
+
+__all__ = [
+            "BasePhysicsPlugin",
+            "BaseFormatPlugin"
+           ]
