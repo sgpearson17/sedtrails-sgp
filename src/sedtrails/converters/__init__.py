1754ddf3936e509fe69a7dc8479af4775b1b7ad3
Manuel Garcia <gilbert.mg22@gmail.com>
2025-02-19 14:58:54 +0100

    add BasePhysicsPlugin and BaseFormatPlugin to converters module

--- src/sedtrails/converters/__init__.py
+++ src/sedtrails/converters/__init__.py
@@ -1,1 +1,7 @@
-#populate this file
+from .plugins.physics.plugin import BasePhysicsPlugin
+from .plugins.format.plugin import BaseFormatPlugin
+
+__all__ = [
+            "BasePhysicsPlugin",
+            "BaseFormatPlugin"
+           ]
