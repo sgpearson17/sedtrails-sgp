dbb8653c7e8c7204b46ccd96912c1afc7d256507
Manuel Garcia <gilbert.mg22@gmail.com>
2025-02-26 19:09:52 +0100

    add initial structure and placeholders for lagrangian particle tracer.

---
+++ src/sedtrails/particle_tracer/data_retriever.py
@@ -0,0 +1,5 @@
+"""
+Flow Field Data Retriever
+=========================
+Retrieve flow field data from the transport converter or the simulation chaching and state tracker.
+"""
---
+++ src/sedtrails/particle_tracer/time_processing_interface.py
@@ -0,0 +1,5 @@
+"""
+Processing Time Interface
+=========================
+Privides access to start/end times of routinges in the Lagrangian Particle Tracer.
+"""
