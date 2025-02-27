94221cdfcc4bed02793c20caa548a77f72ca2e5f
Manuel Garcia <gilbert.mg22@gmail.com>
2025-02-27 15:17:25 +0100

    fix: correct typo in simulation_buffer.py documentation

--- src/sedtrails/data_manager/simulation_buffer.py
+++ src/sedtrails/data_manager/simulation_buffer.py
@@ -1,5 +1,5 @@
 """
 Simulation Data Buffer
 ======================
-Temporarly stores chuncks of simulation data in memory, while they wait to be written to a simulation file.
+Temporarily stores chunks of simulation data in memory, while they wait to be written to a simulation file.
 """
