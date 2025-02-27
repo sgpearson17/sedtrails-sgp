4143fcd74ef222d3ffc5b3bfd5399189ae39a8b7
Manuel Garcia <gilbert.mg22@gmail.com>
2025-02-27 15:16:48 +0100

    fix: correct typo in log_buffer.py documentation

--- src/sedtrails/data_manager/log_buffer.py
+++ src/sedtrails/data_manager/log_buffer.py
@@ -1,6 +1,6 @@
 """
 Log Data Buffer
 ===============
-Temporarly stores chuncks of logs in memorey, while they 
+Temporarily stores chunks of logs in memory, while they 
 wait to be written to a log file.
 """
