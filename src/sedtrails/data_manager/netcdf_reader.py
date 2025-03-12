e16d4bd429bfb96dbaa9c6004cbc3d37eaddad6a
Manuel Garcia <gilbert.mg22@gmail.com>
2025-03-12 14:24:27 +0100

    chore: add example usage for xugrid in NetCDF reader

--- src/sedtrails/data_manager/netcdf_reader.py
+++ src/sedtrails/data_manager/netcdf_reader.py
@@ -3,3 +3,16 @@ NetCDF Reader
 
 Reads NetCDF files produced by the SedTrails Particle Tracer System.
 """
+
+import xugrid as xu
+
+ds = xu.data.adh_san_diego(xarray=True)
+print(ds)
+
+uds = xu.UgridDataset(ds)
+print(uds)
+
+elev = uds['elevation']
+print(elev)
+
+elev.plot()
