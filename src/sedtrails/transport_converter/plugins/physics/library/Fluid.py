da5a57b566636b0f397de6fd6839516f7a1b9b96
Natascia Pannozzo <natasciapannozzo@gmail.com>
2025-04-01 15:09:48 +0200

    Modified Particle classes based on discussion regarding internal data structure and added more extensive comments in both files.

--- src/sedtrails/transport_converter/plugins/physics/library/Fluid.py
+++ src/sedtrails/transport_converter/plugins/physics/library/Fluid.py
@@ -1,3 +1,7 @@
+"""
+Class representing Fluid physical properties used by the transport converter.
+"""
+
 from dataclasses import dataclass
 import numpy as np
 
@@ -6,35 +10,48 @@ import numpy as np
 class Fluid:
     """
     For example, water and wind.
+    
+    Attributes
+    ----------
+    abstau_mean : float
+        absolute mean shear stress of the fluid [N/m^2].
+    abstau_max : float
+        absolute maximum shear stress of the fluid [N/m^2].
+    U_mag : float
+        total velocity magnitude of the fluid [m/s].
+    ustar_mean : float
+        mean friction velocity of the fluid [m/s].
+    ustar_max : float
+        maximum friction velocity of the fluid [m/s].
     """
     
     # concrete method
     def abstau_mean(self, D) -> float:
         """
-        Calculate the absolute mean shear stress [N/m^2] of the fluid.
+        Calculates the absolute mean shear stress of the fluid [N/m^2].
         """
         return np.abs(D.tau_mean)
     
     def abstau_max(self, D) -> float:
         """
-        Calculate the absolute maximum shear stress [N/m^2] of the fluid.
+        Calculates the absolute maximum shear stress of the fluid [N/m^2].
         """
         return np.abs(D.tau_max)
     
     def U_mag(self, Ux, Uy) -> float:
         """
-        Calculate the total velocity magnitude [m/s] of the fluid.
+        Calculates the total velocity magnitude of the fluid [m/s].
         """
         return np.sqrt(Ux ** 2 + Uy ** 2)
     
     def ustar_mean(self, abstau_mean, S) -> float:
         """
-        Calculate the mean friction velocity [m/s] of the fluid.
+        Calculates the mean friction velocity of the fluid [m/s].
         """
         return np.sqrt(abstau_mean / S.rhoFluid)
 
     def ustar_max(self, abstau_max, S) -> float:
         """
-        Calculate the maximum friction velocity [m/s] of the fluid.
+        Calculates the maximum friction velocity of the fluid [m/s].
         """
         return np.sqrt(abstau_max / S.rhoFluid)
--- src/sedtrails/transport_converter/plugins/physics/library/Particle.py
+++ src/sedtrails/transport_converter/plugins/physics/library/Particle.py
@@ -1,3 +1,7 @@
+"""
+Classes representing particle physical properties used by the transport converter.
+"""
+
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
 import numpy as np
@@ -6,244 +10,131 @@ import numpy as np
 # Abstract class
 class Particle(ABC):
     """
-    For example, passive, sediment and biological.
+    Class representing particle physical properties used by the transport converter
+    for all particles (for example, passive, sand and mud).
+    
+    Attributes
+    ----------
+    Dstar_Tracer : float
+        diamentionless diameter of the tracer sediment particle [m].
+    Dstar_Background : float
+        diamentionless diameter of the background sediment particle [m].
+    ratio : float
+        ratio of tracer sediment particle diameter to background sediment particle diameter [-].
     """
 
-    @abstractmethod
-    def Dstar_Tracer(self) -> float:
-        """
-        Calculate the diamentionless diameter [m] of the tracer particle.
-        """
-        pass
-
-    def Dstar_Background(self) -> float:
-        """
-        Calculate the diamentionless diameter [m] of the background particle.
-        """
-        pass
-
-    def w_s_t(self) -> float:
-        """
-        Calculate the settling velocity [m/s] of the tracer particle.
-        """
-        pass
-
-    def theta_cr_Tracer(self) -> float:
-        """
-        Calculate the critical Shields number [-] for the tracer particle.
-        """
-        pass
-
-    def theta_cr_Background(self) -> float:
-        """
-        Calculate the critical Shields number [-] for the background particle.
-        """
-        pass
-
-    def ratio(self) -> float:
-        """
-        Calculate the ratio of tracer particle diameter [-] to background particle diameter.
-        """
-        pass
-
-    def theta_cr_Tracer_exp(self) -> float:
-        """
-        Calculate the adsjusted critical Shields number [-] for the tracer particle to
-        take into account hiding/exposure when having a diameter different than the background particles.
-        """
-        pass
-
-    def ustar_cr(self) -> float:
-        """
-        Calculate the critical friction velocity [m/s] for particle erosion in the fluid.
-        """
-        pass
-
-    def theta_max_Tracer(self) -> float:
-        """
-        Calculate the maximum Shields number [-] for the tracer particle in the fluid.
-        """
-        pass
-
-    def theta_max_Background(self) -> float:
-        """
-        Calculate the maximum Shields number [-] for the background particle in the fluid.
-        """
-        pass
-
-    def rouse(self) -> float:
-        """
-        Calculate the Rouse number [-] for the tracer particle in the fluid.
-        """
-        pass
-
-    def U_bed(self) -> float:
-        """
-        Calculate the bedload velocity [m/s] of the tracer particle in the fluid.
-        """
-        pass
-
-
-class Sediment(Particle):
-    """
-    Class representing sediment particles properties.
-    """
+# I am not quite sure if we should include a class for "Passive(Particle)" here
+# because it does not have any physical properties.
 
     # concrete method
     def Dstar_Tracer(self, S) -> float:
         """
-        Calculate the diamentionless diameter [m] of the tracer sediment particle.
+        Calculates the diamentionless diameter of the tracer sediment particle [m].
         """
         return (S.g * (S.rhoParticle / S.rhoFluid - 1) / (S.visc_kin ** 2)) ** (1/3) * S.dTracer
 
     def Dstar_Background(self, S) -> float:
         """
-        Calculate the diamentionless diameter [m] of the background sediment particle.
+        Calculates the diamentionless diameter of the background sediment particle [m].
         """
         return (S.g * (S.rhoParticle / S.rhoFluid - 1) / (S.visc_kin ** 2)) ** (1/3) * S.dBackground
 
     def ratio(self, S) -> float:
         """
-        Calculate the ratio of tracer sediment particle diameter [-] to background sediment particle diameter.
+        Calculates the ratio of tracer sediment particle diameter to background sediment particle diameter [-].
         """
         return S.dTracer / S.dBackground
 
-class Sand(Sediment(Particle)):
+
+@dataclass
+class Sand(Particle):
     """
-    Class representing sand particles properties.
+    Class representing particle physical properties used by the transport converter for sand.
+    
+    Attributes
+    ----------
+    w_s_t : float
+        settling velocity of the tracer sand particle [m/s].
+    theta_cr_Tracer : float
+        critical Shields number for the tracer sand particle [-].
+    theta_cr_Background : float
+        critical Shields number for the background sand particle [-].
+    theta_cr_Tracer_exp : float
+        adsjusted critical Shields number for the tracer sand particle to
+        take into account hiding/exposure when having a diameter different
+        than the background sand particles [-].
+    ustar_cr : float
+        critical friction velocity for sand particle erosion [m/s].
+    theta_max_Tracer : float
+        maximum Shields number for the tracer sand particle [-].
+    theta_max_Background
+        maximum Shields number for the background sand particle [-].
+    rouse : float
+        Rouse number for the tracer sand particle [-].
+    U_bed : float
+        bedload velocity of the tracer sand particle [m/s].
     """
 
     def w_s_t(self, Dstar_Tracer, S) -> float:
         """
-        Calculate the settling velocity [m/s] of the tracer sand particle.
+        Calculates the settling velocity of the tracer sand particle [m/s].
         """
         return (S.visc_kin / S.dTracer) * (np.sqrt(10.36 ** 2 + 1.049 * (Dstar_Tracer ** 3)) - 10.36)
 
     def theta_cr_Tracer(self, Dstar_Tracer) -> float:
         """
-        Calculate the critical Shields number [-] for the tracer sand particle.
+        Calculates the critical Shields number for the tracer sand particle [-].
         """
         return 0.3 / (1 + 1.2 * Dstar_Tracer) + 0.055 * (1 - np.exp(-0.020 * Dstar_Tracer))
 
     def theta_cr_Background(self, Dstar_Background) -> float:
         """
-        Calculate the critical Shields number [-] for the background sand particle.
+        Calculates the critical Shields number for the background sand particle [-].
         """
         return 0.3 / (1 + 1.2 * Dstar_Background) + 0.055 * (1 - np.exp(-0.020 * Dstar_Background))
 
     def theta_cr_Tracer_exp(self, theta_cr_Tracer, ratio) -> float:
         """
-        Calculate the adsjusted critical Shields number [-] for the tracer sand particle to
-        take into account hiding/exposure when having a diameter different than the background sand particles.
+        Calculates the adsjusted critical Shields number for the tracer sand particle to
+        take into account hiding/exposure when having a diameter different than the background
+        sand particles [-].
         """
         return theta_cr_Tracer * np.sqrt(8 / (3 * (ratio ** 2) + 6 * ratio - 1)) * ((3.2260 * ratio) /\
                     (4 * ratio - 2 * (ratio + 1 - np.sqrt(ratio ** 2 + 2 * ratio - 1/3))))
 
     def ustar_cr(self, S) -> float:
         """
-        Calculate the critical friction velocity [m/s] for sand particle erosion in the fluid.
+        Calculates the critical friction velocity for sand particle erosion [m/s].
         """
         return np.sqrt(S.tau_cr / S.rhoFluid)
 
     def theta_max_Tracer(self, abstau_max, S) -> float:
         """
-        Calculate the maximum Shields number [-] for the tracer sand particle in the fluid.
+        Calculates the maximum Shields number for the tracer sand particle [-].
         """
         return abstau_max / (S.g * (S.rhoParticle - S.rhoFluid) * S.dTracer)
 
     def theta_max_Background(self, abstau_max, S) -> float:
         """
-        Calculate the maximum Shields number [-] for the background sand particle in the fluid.
+        Calculates the maximum Shields number for the background sand particle [-].
         """
         return abstau_max / (S.g * (S.rhoParticle - S.rhoFluid) * S.dBackground)
 
     def rouse(self, w_s_t, ustar_max) -> float:
         """
-        Calculate the Rouse number [-] for the tracer sand particle in the fluid.
+        Calculates the Rouse number for the tracer sand particle [-].
         """
         return w_s_t / (0.4 * ustar_max)
 
     def U_bed(self, ustar_mean, theta_cr_Tracer_exp, theta_max_Tracer) -> float:
         """
-        Calculate the bedload velocity [m/s] of the tracer sand particle in the fluid.
+        Calculates the bedload velocity of the tracer sand particle [m/s].
         """
         return 10 * ustar_mean * (1 - 0.7 * np.sqrt(theta_cr_Tracer_exp / theta_max_Tracer))
 
-class Mud(Sediment(Particle)):
+
+@dataclass
+class Mud(Particle):
     """
-    Class representing mud particles properties.
-    """
-    def w_s_t(self) -> float:
-        """
-        Calculate the settling velocity [m/s] of the tracer mud particle.
-        """
-        return
-
-    def theta_cr_Tracer(self) -> float:
-        """
-        Calculate the critical Shields number [-] for the tracer mud particle.
-        """
-        return
-
-    def theta_cr_Background(self) -> float:
-        """
-        Calculate the critical Shields number [-] for the background mud particle.
-        """
-        return
-
-    def theta_cr_Tracer_exp(self) -> float:
-        """
-        Calculate the adsjusted critical Shields number [-] for the tracer mud particle to
-        take into account hiding/exposure when having a diameter different than the background mud particles.
-        """
-        return
-
-    def ustar_cr(self) -> float:
-        """
-        Calculate the critical friction velocity [m/s] for mud particle erosion in the fluid.
-        """
-        return
-
-    def theta_max_Tracer(self) -> float:
-        """
-        Calculate the maximum Shields number [-] for the tracer mud particle in the fluid.
-        """
-        return
-
-    def theta_max_Background(self) -> float:
-        """
-        Calculate the maximum Shields number [-] for the background mud particle in the fluid.
-        """
-        return
-
-    def rouse(self) -> float:
-        """
-        Calculate the Rouse number [-] for the tracer mud particle in the fluid.
-        """
-        return
-
-    def U_bed(self) -> float:
-        """
-        Calculate the bedload velocity [m/s] of the tracer mud particle in the fluid.
-        """
-        return
-
-class Biological(Particle):
-    """
-    Class representing biologcal particles properties.
-    """
-
-class Propagule(Biological(Particle)):
-    """
-    Class representing mangrove propagules properties.
-    """
-
-class Larva(Biological(Particle)):
-    """
-    Class representing coral larvae properties.
-    """
-
-class eDNA(Biological(Particle)):
-    """
-    Class representing eDNA properties.
+    Placeholder for class representing particle physical properties used by the transport converter for mud.
     """
