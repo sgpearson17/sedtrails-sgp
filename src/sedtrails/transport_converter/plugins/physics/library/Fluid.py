9c69d8f7f5d4ccdf55de9c5bae4887f1015fcc41
Manuel Garcia <gilbert.mg22@gmail.com>
2025-05-14 14:26:23 +0200

    Fix attribute names and improve docstring formatting in Fluid class

--- src/sedtrails/transport_converter/plugins/physics/library/Fluid.py
+++ src/sedtrails/transport_converter/plugins/physics/library/Fluid.py
@@ -1,5 +1,5 @@
 """
-Class representing Fluid physical properties used by the transport converter.
+Class representing a Fluid physical properties used by the transport converter.
 """
 
 from dataclasses import dataclass
@@ -10,40 +10,54 @@ import numpy as np
 class Fluid:
     """
     For example, water and wind.
-    
+
     Attributes
     ----------
-    abstau_mean : float
+    abs_shear_mean : float
         absolute mean shear stress of the fluid [N/m^2].
-    abstau_max : float
+    abs_shear_max : float
         absolute maximum shear stress of the fluid [N/m^2].
-    U_mag : float
+    velocity_magnitud : float
         total velocity magnitude of the fluid [m/s].
-    ustar_mean : float
+    friction_mean : float
         mean friction velocity of the fluid [m/s].
-    ustar_max : float
+    friction_max : float
         maximum friction velocity of the fluid [m/s].
     """
-    
+
+    abs_shear_mean: float = None
+    abs_shear_max: float = 0.0
+    velocity_magnitude: float = 0.0
+    friction_mean: float = 0.0
+    friction_max: float = 0.0
+
+
+class FluidPhysics:
+    """
+    Class for storing physics converted values of a Fluid.
+    """
+
+    fluid: Fluid = None
+
     # concrete method
     def abstau_mean(self, D) -> float:
         """
         Calculates the absolute mean shear stress of the fluid [N/m^2].
         """
         return np.abs(D.tau_mean)
-    
+
     def abstau_max(self, D) -> float:
         """
         Calculates the absolute maximum shear stress of the fluid [N/m^2].
         """
         return np.abs(D.tau_max)
-    
+
     def U_mag(self, Ux, Uy) -> float:
         """
         Calculates the total velocity magnitude of the fluid [m/s].
         """
-        return np.sqrt(Ux ** 2 + Uy ** 2)
-    
+        return np.sqrt(Ux**2 + Uy**2)
+
     def ustar_mean(self, abstau_mean, S) -> float:
         """
         Calculates the mean friction velocity of the fluid [m/s].
