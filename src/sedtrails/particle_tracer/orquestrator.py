65aeab721ec6071dbc467bc5d81d02aaf7a0e89a
Manuel Garcia <gilbert.mg22@gmail.com>
2025-05-21 17:03:37 +0200

    feat: add particle tracing orchestrator documentation

---
+++ src/sedtrails/particle_tracer/orquestrator.py
@@ -0,0 +1,7 @@
+"""
+Particle Tracing orchestrator
+
+Orchestrates the parallel execution fo particle tracing routines using a multi-process strategy.
+A process should be created for each particle to be traced.
+"""
+
