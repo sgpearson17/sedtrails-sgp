319b97b9a94e8886aefdbbbdac8877fe104b8270
Manuel Garcia <gilbert.mg22@gmail.com>
2025-05-27 22:53:19 +0200

    add todo

--- src/sedtrails/particle_tracer/orquestrator.py
+++ src/sedtrails/particle_tracer/orquestrator.py
@@ -5,3 +5,4 @@ Orchestrates the parallel execution fo particle tracing routines using a multi-p
 A process should be created for each particle to be traced.
 """
 
+# TODO: Continue here, use the example of a single particle to defin the general structure of the orchestrator
