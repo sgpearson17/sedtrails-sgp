8f9433d5a4b94fe34d534917f6b9cf905ea9f7a9
Manuel Garcia <gilbert.mg22@gmail.com>
2025-06-04 18:09:58 +0200

    Remove hard coded paths to configuation schema

--- src/sedtrails/configuration_interface/configuration_controller.py
+++ src/sedtrails/configuration_interface/configuration_controller.py
@@ -17,7 +17,7 @@ class Controller(ABC):
     Abstract base class for configuration controllers.
     """
 
-    # TODO: Something to keep in mind in this class is that
+    # TODO: Something to keep in mind in this class:
     # if we can and it is convenient to have separted methods to retrieve parts of the configuration
     # that go to the particle tracer and the transport converter. For example, if it is convenient to
     # have a method that returns the configuration values that are only relevant for the particle tracer.
@@ -55,7 +55,9 @@ class ConfigurationController(Controller):
             The path to the configuration file to read.
         """
 
-        with pkg_resources.as_file(pkg_resources.files('sedtrails.config').joinpath('schema.json')) as config_schema:
+        with pkg_resources.as_file(
+            pkg_resources.files('sedtrails.config').joinpath('main.schema.json')
+        ) as config_schema:
             if not os.path.exists(config_schema):
                 raise FileNotFoundError(
                     f'SedTRAILS validation schema file not found: {config_schema}. \
