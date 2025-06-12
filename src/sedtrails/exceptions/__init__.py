e5e85f3706f83ade7aaf64bc612cc529e44f6995
Manuel Garcia <gilbert.mg22@gmail.com>
2025-06-12 11:21:15 +0200

    refactor(validator): raised proper errors when yaml validation failed. Update unit tests or YAMLConfigValidator

--- src/sedtrails/exceptions/__init__.py
+++ src/sedtrails/exceptions/__init__.py
@@ -1,5 +1,15 @@
 """Custom exceptions go in this directory."""
 
-from .exceptions import SedtrailsException, YamlParsingError, YamlOutputError, YamlValidationError
+from .exceptions import (
+    SedtrailsException,
+    YamlParsingError,
+    YamlOutputError,
+    YamlValidationError,
+)
 
-__all__ = ['SedtrailsException', 'YamlParsingError', 'YamlOutputError', 'YamlValidationError']
+__all__ = [
+    'SedtrailsException',
+    'YamlParsingError',
+    'YamlOutputError',
+    'YamlValidationError',
+]
