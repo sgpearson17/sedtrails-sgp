f7a8cd4f23f2bf8cd5a255ed32b2e62b5ccb830c
Manuel Garcia <gilbert.mg22@gmail.com>
2025-06-29 13:00:39 +0200

    Organize __init__.py: add imports and define __all__ for public API

--- src/sedtrails/transport_converter/__init__.py
+++ src/sedtrails/transport_converter/__init__.py
@@ -1,1 +1,11 @@
 """A directory for the Transport Converter"""
+
+from .sedtrails_data import SedtrailsData
+from .physics_converter import PhysicsConverter
+from .format_converter import FormatConverter
+
+__all__ = [
+    'SedtrailsData',
+    'PhysicsConverter',
+    'FormatConverter',
+]
