ebdabf0e6b3b3b0b9476ffea34f60ec441952d27
Manuel Garcia <gilbert.mg22@gmail.com>
2025-07-03 12:09:27 +0200

    Refactor __init__.py: organize imports and define __all__ for public API

--- src/sedtrails/data_manager/__init__.py
+++ src/sedtrails/data_manager/__init__.py
@@ -1,1 +1,10 @@
 """A directory for the Data Management Module"""
+
+from .netcdf_reader import NetCDFReader
+from .netcdf_writer import NetCDFWriter
+
+
+__all__ = [
+    'NetCDFReader',
+    'NetCDFWriter',
+]
