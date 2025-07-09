db3662eef7225118a7a0c81e365a8f36b429fb99
Manuel Garcia <gilbert.mg22@gmail.com>
2025-07-09 07:02:13 +0200

    Add SimulationDataBuffer and MemoryManager to exports in data_manager module

--- src/sedtrails/data_manager/__init__.py
+++ src/sedtrails/data_manager/__init__.py
@@ -2,9 +2,13 @@
 
 from .netcdf_reader import NetCDFReader
 from .netcdf_writer import NetCDFWriter
+from .simulation_buffer import SimulationDataBuffer
+from .memory_manager import MemoryManager
 
 
 __all__ = [
     'NetCDFReader',
     'NetCDFWriter',
+    'SimulationDataBuffer',
+    'MemoryManager',
 ]
