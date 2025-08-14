6a90f35667edc46f02a48e23a046db2c02a9ae16
Manuel Garcia <gilbert.mg22@gmail.com>
2025-08-14 16:41:06 +0200

    refactor: add seed parameter to random configuration for RandomStrategy

--- tests/particle_tracer/test_particle_seeder.py
+++ tests/particle_tracer/test_particle_seeder.py
@@ -91,7 +91,7 @@ def random_config():
             'population': {
                 'particle_type': 'sand',
                 'seeding': {
-                    'strategy': {'random': {'bbox': '1.0,2.0, 3.0,4.0'}},
+                    'strategy': {'random': {'bbox': '1.0,2.0, 3.0,4.0', 'seed': 42}},
                     'quantity': 5,
                     'release_start': '2025-06-18 13:00:00',
                 },
