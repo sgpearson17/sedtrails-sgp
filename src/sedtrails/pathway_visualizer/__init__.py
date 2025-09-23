52de9ce0e268dc7dbd7f7bc6cc39f2870488e377
Manuel Garcia <gilbert.mg22@gmail.com>
2025-09-24 00:28:10 +0200

    move NetCDFInspector to top level module

--- src/sedtrails/pathway_visualizer/__init__.py
+++ src/sedtrails/pathway_visualizer/__init__.py
@@ -1,6 +1,5 @@
 """A directory for the Pathway Visualizer"""
 
 from .simulation_dashboard import SimulationDashboard
-from .nc_inspector import NetCDFInspector
 
-__all__ = ['SimulationDashboard', 'NetCDFInspector']
+__all__ = ['SimulationDashboard']
