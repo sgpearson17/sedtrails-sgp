c74ed2a70de4e45619401d0a1b17eba5687026b9
Manuel Garcia <gilbert.mg22@gmail.com>
2025-09-23 17:15:03 +0200

    feat: add NetCDFInspector to __all__ in pathway_visualizer

--- src/sedtrails/pathway_visualizer/__init__.py
+++ src/sedtrails/pathway_visualizer/__init__.py
@@ -1,5 +1,6 @@
 """A directory for the Pathway Visualizer"""
 
 from .simulation_dashboard import SimulationDashboard
+from .nc_inspector import NetCDFInspector
 
-__all__ = ['SimulationDashboard']
+__all__ = ['SimulationDashboard', 'NetCDFInspector']
